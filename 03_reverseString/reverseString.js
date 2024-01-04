8fe36077cb014735c323e7d6746db2e507a5e4a0
William Langley <138311847+wlangley7870@users.noreply.github.com>
2024-01-03 20:59:49 -0600

    03_reverseString Exercise Complete
    code passes all tests, and exercises have been completed for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    const inText = string;
+    let outText = "";
+    
+    for(let i = inText.length; i > 0; i--) {
+
+        outText += inText.charAt(i-1)
+        
+    } 
+    return outText
 
 };
 
