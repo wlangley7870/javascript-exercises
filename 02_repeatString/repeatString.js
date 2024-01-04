d46853692a60a04c46ad08a0b0a3d23e85c9e2a5
William Langley <138311847+wlangley7870@users.noreply.github.com>
2024-01-04 00:05:59 -0600

    Exercises complete for 01_helloWorld and 02_repeatString
    exercises have been completed and pass all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let output = "";
+
+    if(num < 0){
+        return 'ERROR'
+    }
+
+    for(let i = 0; i<num; i++) {
+        output += string;
+    }
+
+    return output
 
 };
 
